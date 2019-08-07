6a507090d4521d08bbc92e39e6d927d841f677fa
Muhammad Muhajir <muhajirframe@gmail.com>
2019-08-07 19:58:40 +0700

    add link to header

--- components/headers.tsx
+++ components/headers.tsx
@@ -1,10 +1,15 @@
 import { TITLE, TWITTER_URL } from "../config";
 const TWITTER_LOGO_PATH = "/static/twitter.svg";
+import Link from 'next/link'
 
 const Header = () => (
   <header className="bg-dark-light absolute w-full shadow-lg">
     <nav className="max-w-6xl mx-auto py-8 font-black text-xl flex justify-between">
-      <h1 className="text-accent">{TITLE}</h1>
+      <Link href="/">
+        <a>
+          <h1 className="text-accent">{TITLE}</h1>
+        </a>
+      </Link>
       <a href={TWITTER_URL} className="w-8">
         <img src={TWITTER_LOGO_PATH} />
       </a>
