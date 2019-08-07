7b83be20cd8089aab803bd198899e9a6c08265f9
Muhammad Muhajir <muhajirframe@gmail.com>
2019-08-07 21:12:41 +0700

    solve key problem

--- components/TipsCollection.tsx
+++ components/TipsCollection.tsx
@@ -6,7 +6,7 @@ const getTipsLink = (slug: string) => `t/${slug}`;
 const TipsCollection = () => (
   <div className="flex flex-wrap -mx-8">
     {data.tips.map(t => (
-      <Link href={getTipsLink(t.slug)}>
+      <Link href={getTipsLink(t.slug)} key={t.slug}>
           <a className="text-white w-1/3 px-8 pb-12">
             <img src={t.gif} className="shadow-xl rounded" />
             <div className="mt-4">{t.title}</div>
