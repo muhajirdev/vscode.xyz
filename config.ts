180199584072d62662707d5d21b1f3ca2e5338fe
Muhammad Muhajir <muhajirframe@gmail.com>
2019-08-07 20:25:28 +0700

    update config

--- config.ts
+++ config.ts
@@ -1,3 +1,5 @@
-export const TWITTER_URL = 'https://twitter.com/vscodexyz'
-export const AUTHOR_TWITTER_URL = 'https://twitter.com/muhajirdev'
+export const TWITTER_URL = "https://twitter.com/vscodexyz";
+export const AUTHOR_TWITTER_URL = "https://twitter.com/muhajirdev";
 export const TITLE = "VSCode.XYZ";
+export const GITHUB_ISSUE_URL =
+  "https://github.com/muhajirdev/vscode.xyz/issues/new";
