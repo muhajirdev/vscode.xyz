7087730ea451400fec434f62b7e42b3c1630384e
Muhammad Muhajir <muhajirframe@gmail.com>
2019-08-07 20:25:39 +0700

    update detail page styling

--- pages/t/[id].tsx
+++ pages/t/[id].tsx
@@ -5,7 +5,7 @@ import { ITips, IPlugin, IContributor } from "../../types";
 import Layout from "../../components/layout";
 import data from "../../data.json";
 
-import { TWITTER_URL } from "../../config";
+import { TWITTER_URL, GITHUB_ISSUE_URL } from "../../config";
 const NotFound = () => (
   <section className="max-w-6xl mx-auto flex py-32">
     <div className="w-1/2">
@@ -40,7 +40,7 @@ const Plugins: React.FC<{ plugins: string[] }> = props => (
     <ul>
       {props.plugins.map(p => (
         <li className="ml-8 my-2" key={p}>
-          <a className="text-white text-lg" href={getPlugin(p).url}>
+          <a className="text-gray-200 text-lg font-bold" href={getPlugin(p).url}>
             {getPlugin(p).name}
           </a>
         </li>
@@ -68,7 +68,7 @@ const Contributors: React.FC<{ contributors: string[] }> = props => (
     <ul>
       {props.contributors.map(p => (
         <li className="ml-8 my-2" key={p}>
-          <a className="text-white text-lg" href={getContributor(p).url}>
+          <a className="text-gray-200 text-lg font-bold" href={getContributor(p).url}>
             {getContributor(p).name}
           </a>
         </li>
@@ -79,17 +79,33 @@ const Contributors: React.FC<{ contributors: string[] }> = props => (
 
 const Tips: React.FC<ITips> = props => {
   return (
-    <section className="max-w-6xl mx-auto flex py-32">
-      <div className="w-1/2">
-        <img src={props.gif} />
-      </div>
-      <div className="w-1/2 pl-8">
-        <h1 className="text-white font-bold text-4xl mb-8">{props.title}</h1>
-        <h2 className="text-gray-200 text-xl">{props.description}</h2>
-        <Plugins plugins={props.plugins} />
-        <Contributors contributors={props.contributors} />
-      </div>
-    </section>
+    <div>
+      <section className="max-w-6xl mx-auto flex pt-24">
+        <div className="w-2/3 pr-8">
+          <img src={props.gif} className="w-full h-auto shadow-2xl" />
+        </div>
+        <div className="w-1/3">
+          <h1 className="text-white font-bold text-4xl mb-8">{props.title}</h1>
+          <h2 className="text-gray-200 text-xl font-bold">{props.description}</h2>
+          <Plugins plugins={props.plugins} />
+          <Contributors contributors={props.contributors} />
+        </div>
+      </section>
+      <section className="max-w-6xl mx-auto py-24">
+        <div className="max-w-2xl">
+          <h3 className="font-bold text-white text-3xl">
+            Want to submit a tips?
+          </h3>
+          <h4 className="font-bold text-gray-400 text-lg">
+            Mention us on <a className="underline" href={TWITTER_URL}>twitter</a> or open an issue on
+            our{" "}
+            <a className="underline" href={GITHUB_ISSUE_URL}>
+              github repo
+            </a>
+          </h4>
+        </div>
+      </section>
+    </div>
   );
 };
 
